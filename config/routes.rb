2eb86c66d13173bd86f693119e0162dd383f84c9
Joshua Wang <joshuawang3110@gmail.com>
2017-12-30 22:06:15 -0500

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'courses#index'
 
-  get 'prereq_courses' => 'courses#search_prereq_courses'
+  get 'prereq-courses' => 'courses#search_prereq_courses'
 
 end
