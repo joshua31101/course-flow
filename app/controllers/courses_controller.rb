6c445d531a89b05e7bed9ceaa518643b5ecc09f8
Joshua Wang <joshuawang3110@gmail.com>
2017-12-30 22:04:54 -0500

    Update json prereq course search for and/or

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,13 +6,13 @@ class CoursesController < ApplicationController
   end
 
   # GET
-  # URL: /prereq_courses?course_name=
+  # URL: /prereq-courses?course_name=
   # Find prerequisite courses from the course_name passed in from search input bar
   def search_prereq_courses
     if params[:course_name].present?
       major = params[:course_name].split(' ')[0].upcase
       course_number = params[:course_name].split(' ')[1]
-      render json: get_user_course_prereqs("#{major} #{course_number}", nil), status: :ok
+      render json: get_user_course_prereqs("#{major} #{course_number}"), status: :ok
     else
       render json: 'Opps.. we couldn\'t find a course. Try typing something like CS 3510', status: :forbidden
     end
@@ -20,42 +20,56 @@ class CoursesController < ApplicationController
 
   private
 
+
   # @param course_name String - 'major course_number' ex) 'CS 1301'
   # @return course_prereqs Hash - course root parent
-  def get_user_course_prereqs(course_name, openup)
+  def get_user_course_prereqs(course_name)
     if course_name.blank?
-      return nil
+      return
     end
 
-    course_info = course_name.split(' ')
-    major = course_info[0]
-    course_num = course_info[1]
+    if course_name.class == String
+      course_info = course_name.split(' ')
+      major = course_info[0]
+      course_num = course_info[1]
 
-    if major.present? && course_num.present?
-      _course_info = JsonHelper.courses[major.to_sym][course_num.to_sym]
-      if _course_info
-        json_prereq_courses = {
-          course_name: course_name,
-          course_title: _course_info[:title],
-          course_desc: _course_info[:desc],
-          course_credit_hour: _course_info[:credit_hour],
-          children: [],
-          openup: openup
-        }
-      else
-        # Return if invalid course
-        return nil
-      end
-    end
-
-    if JsonHelper.courses[major.to_sym][course_num.to_sym] && JsonHelper.courses[major.to_sym][course_num.to_sym][:prereq]
-      JsonHelper.courses[major.to_sym][course_num.to_sym][:prereq].each do |prereq_course|
-        if prereq_course != ' '
-          json_prereq_courses[:children] << get_user_course_prereqs(prereq_course, course_name)
+      if major.present? && course_num.present?
+        if _course_info = CoursesHelper.courses[major.to_sym][course_num.to_sym]
+          json_prereq_courses = {
+            course_name: course_name,
+            course_title: _course_info[:title],
+            course_desc: _course_info[:desc],
+            course_credit_hour: _course_info[:credit_hour],
+            relationship: '',
+            children: []
+          }
+          prereq = _course_info[:prereq]
+          if prereq.present?
+            prereq.each do |rel, courses|
+              json_prereq_courses[:relationship] = rel
+              courses.each do |course|
+                _c = get_user_course_prereqs(course)
+                json_prereq_courses[:children] << _c if _c
+              end
+            end
+          end
+        else
+          # Return nil if invalid course
+          return nil
         end
       end
     else
-      return json_prereq_courses
+      # course_name param is a hash due to nested relationship
+      course_name.each do |rel, courses|
+        json_prereq_courses = {
+          relationship: rel,
+          children: []
+        }
+        courses.each do |course|
+          _c = get_user_course_prereqs(course)
+          json_prereq_courses[:children] << _c if _c
+        end
+      end
     end
 
     return json_prereq_courses
